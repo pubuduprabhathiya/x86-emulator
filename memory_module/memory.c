47c9a76fea7771f35be44522a502c8569e421ef4
kavinduravishka <kavinduravishka98sl@gmail.com>
2023-01-12 15:43:19 +0530

    memery commit

--- memory_module/memory.c
+++ memory_module/memory.c
@@ -7,36 +7,45 @@
 struct Memory memobj;
 struct Memory *mem = &memobj;
 
-// uint32_t *addresses[2];
-// uint32_t a1=10,a2=20;
-// addresses[0] = (uint32_t*)&a1;
-// addresses[1] = (uint32_t*)&a2;
-
-// uint64_t *values_to_write[2];
-// uint64_t v1=10,v2=20;
-// values_to_write[0] = (uint64_t*)&v1;
-// values_to_write[1] = (uint64_t*)&v2;
 
 int main(){
 
-    uint32_t *addresses[2];
-    uint32_t a1=10,a2=20;
-    addresses[0] = (uint32_t*)&a1;
-    addresses[1] = (uint32_t*)&a2;
+    uint32_t *addresses[4];
+    uint32_t a1=10,a2=20,a3=30,a4=40;
+    addresses[0] = &a1;
+    addresses[1] = &a2;
+    addresses[2] = &a3;
+    addresses[3] = &a4;
 
-    uint64_t *values_to_write[2];
-    uint64_t v1=0x11111111,v2=0x22222222;
-    values_to_write[0] = (uint64_t*)&v1;
-    values_to_write[1] = (uint64_t*)&v2;
+    uint64_t *values_to_write[4];
+    struct Data d1,d2,d3,d4;
 
+    d1.type=UINT32;
+    d1.value = malloc(sizeof(uint32_t));
+    *(uint32_t*)d1.value=0x11111111;
 
-    printf("0 : %u\n",sizeof(*values_to_write));
-    printf("size uint8 : %i\n\n",sizeof(uint8_t));
-    // printf("Size of uint64 : %i\n",sizeof(uint64_t));
-    // printf("Size of values_to_write array : %i\n",sizeof(values_to_write[0]));
-    
-    mem->hashtable = (uint32_t*)malloc(sizeof(uint32_t));
-    mem->values = (uint64_t*)malloc(sizeof(uint64_t));
+    d2.type=UINT64;
+    d2.value = malloc(sizeof(uint64_t));
+    *(uint64_t*)d2.value = 0x222222222222222;
+
+    d3.type=UINT8;
+    d3.value = malloc(sizeof(uint8_t));
+    *(uint8_t*)d3.value = 0x33;
+
+    d4.type=UINT16;
+    d4.value = malloc(sizeof(uint16_t));
+    *(uint16_t*)d4.value = 0x33;
+
+    values_to_write[0] = &d1;
+    values_to_write[1] = &d2;
+    values_to_write[2] = &d3;
+    values_to_write[3] = &d4;
+
+    mem->hashtable = (uint32_t**)malloc(sizeof(uint32_t));
+    mem->values = (uint64_t**)malloc(sizeof(uint64_t));
+    memset(mem->hashtable,0,sizeof(uint32_t)*1);
+    memset(mem->values,0,sizeof(uint64_t)*1);
+    mem->hashtablesize=0;
 
     mem->cell_index = cell_index;
     mem->dump = dump;
@@ -44,7 +53,7 @@ int main(){
     mem->read = read;
     mem->write = write;
 
-    for(int i=0;i<2;i++){
+    for(int i=0;i<4;i++){
         mem->write(addresses[i],values_to_write[i],mem);
     }
 
